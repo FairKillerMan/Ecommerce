19d5c827a5e2a7221595712acd3405a215dab5c7
Thiago Oliveira da Silva <thiago.oliveira.gouro@hotmail.com>
2021-11-17 12:10:21 -0300

    Documento da API e testes: instalação do pacote swashbuckle, configuração do swagger e adicionando o launchUrl.

--- src/services/GRUPO.Identidade.API/Startup.cs
+++ src/services/GRUPO.Identidade.API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace GRUPO.Identidade.API
 {
@@ -29,10 +31,29 @@ namespace GRUPO.Identidade.API
                 .AddDefaultTokenProviders();
 
             services.AddControllers ();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(name: "v1", new OpenApiInfo
+                {
+                    Title = "Grupo Grão de Ouro Identity API",
+                    Description = "Essa API faz parte do curso ASP.NET Core Enterprise Applications",
+                    Contact = new OpenApiContact() { Name = "Thiago Silva", Email = "thiagooliveirasilva.bcc@gmail.com"},
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")},
+
+
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint(url:"/swagger/v1/swagger.json", name:"v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
